d54a5c17e005d5db42f7a8b32f147ef0b5197223
ThatMadMf <maxdarksiders@gmail.com>
2024-10-09 19:48:49 +0300

    Introduce BenchmarkResult model

---
+++ app/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, DateTime
+
+from .database import Base
+
+
+class BenchmarkResult(Base):
+    __tablename__ = 'benchmark_results'
+
+    id = Column(Integer, primary_key=True)
+    request_id = Column(String, index=True)
+    prompt_text = Column(String)
+    generated_text = Column(String)
+    token_count = Column(Integer)
+    time_to_first_token = Column(Integer)
+    time_per_output_token = Column(Integer)
+    total_generation_time = Column(Integer)
+    timestamp = Column(DateTime)
