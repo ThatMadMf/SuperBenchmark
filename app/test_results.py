0b2743b36c3b30765c9047754f5156ac7407dd2c
ThatMadMf <maxdarksiders@gmail.com>
2024-10-09 19:50:16 +0300

    Add tests

---
+++ app/test_results.py
@@ -0,0 +1,61 @@
+import os
+
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+client = TestClient(app)
+
+
+def test_get_average__success():
+    os.environ["SUPERBENCHMARK_DEBUG"] = "True"
+
+    response = client.get("/results/average/")
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "average_token_count": 10.2,
+        "average_time_to_first_token": 216.0,
+        "average_time_per_output_token": 27.6,
+        "average_total_generation_time": 485.2
+    }
+
+    del os.environ["SUPERBENCHMARK_DEBUG"]
+
+
+def test_get_average__not_available():
+    response = client.get("/results/average/")
+
+    assert response.status_code == 503
+
+
+def test_get_average_range__success():
+    os.environ["SUPERBENCHMARK_DEBUG"] = "True"
+
+    response = client.get("/results/average/2024-06-01T12:00:00/2024-06-01T13:00:00")
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "average_token_count": 5.5,
+        "average_time_to_first_token": 175.0,
+        "average_time_per_output_token": 27.5,
+        "average_total_generation_time": 325.0
+    }
+
+    del os.environ["SUPERBENCHMARK_DEBUG"]
+
+
+def test_get_average_range__no_content():
+    os.environ["SUPERBENCHMARK_DEBUG"] = "True"
+
+    response = client.get("/results/average/2024-06-01T10:00:00/2024-06-01T11:00:00")
+
+    assert response.status_code == 204
+
+    del os.environ["SUPERBENCHMARK_DEBUG"]
+
+
+def test_get_average_range__not_available():
+    response = client.get("/results/average/2024-06-01T12:00:00/2024-06-01T13:00:00")
+
+    assert response.status_code == 503
